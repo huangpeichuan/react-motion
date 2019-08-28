47e122074cbbac7ec133c8dc3c2cfb2371e2101f
leesin <1044992077@qq.com>
2019-08-28 16:21:22 +0800

    this is a test for "vscode commit to git "

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = 'development1';
 
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
